91650ee9a6e0cdb834e1c1f7d10dccf37c30232f
mykolasmith <mykolasmith@gmail.com>
2012-04-13 16:45:28 -0300

    Update templates/layout.tpl.php

--- templates/layout.tpl.php
+++ templates/layout.tpl.php
@@ -8,23 +8,23 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-	<div id="container">
-		<header>
-	    <h1><?php echo APP_NAME; ?></h1>
-	    <?php if(isset($_SESSION['user'])): ?>
-	    <p>What's up, <i><?php echo $_SESSION['user']['username']; ?></i>?
-	      <?php if($_SESSION['user']['can_create']): ?>
-	      <a href="/create">Add Link</a>
-	      <?php endif; ?>
-	      <a href="/logout">Logout</a>
-	    </p>
-	    <?php else: ?>
-	    <p>Hey there, stranger! <a href="/login">Login</a></p>
-	    <?php endif; ?>
-	    </header>
-		<div id="content">
-	    {{ body }}
-		</div>
+    <div id="container">
+	<header>
+	  <h1><?php echo APP_NAME; ?></h1>
+	  <?php if(isset($_SESSION['user'])): ?>
+	  <p>What's up, <i><?php echo $_SESSION['user']['username']; ?></i>?
+	  <?php if($_SESSION['user']['can_create']): ?>
+	  <a href="/create">Add Link</a>
+	  <?php endif; ?>
+	  <a href="/logout">Logout</a>
+	  </p>
+	  <?php else: ?>
+	  <p>Hey there, stranger! <a href="/login">Login</a></p>
+	  <?php endif; ?>
+	</header>
+	<div id="content">
+	  {{ body }}
 	</div>
+    </div>
   </body>
 </html>
